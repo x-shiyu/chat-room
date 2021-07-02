004315ca80eedd97185895c9ff5509aa50471bb6
yu.shi1 <yu.shi1@china.zhaogang.com>
2021-07-02 17:37:50 +0800

    package.json

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,16 @@
-const { override, addWebpackAlias } = require("customize-cra");
+const {
+  override,
+  addWebpackAlias,
+  addWebpackExternals,
+} = require("customize-cra");
 const path = require("path");
 module.exports = override(
   // 配置路径别名
   addWebpackAlias({
     "@": path.resolve(__dirname, "src"),
     "@@": path.resolve(__dirname, "src/components"),
+  }),
+  addWebpackExternals({
+    react: "react",
   })
 );
